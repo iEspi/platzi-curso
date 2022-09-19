6c5c2350bbf8b35eaaf4371080315e5b6b7040f7
iEspi <felipe.gatica.t@gmail.com>
2022-09-18 21:30:27 -0300

    :rocket: Curso Platzi v0.01
    Primer push del curso platzi

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#save_data').click(function () {
+        $('.alert').show()
+    })
+});
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
